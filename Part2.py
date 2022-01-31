e3150e2eab797e048bf3ed2fd387bde20660478e
h-latta <h.latta@yahoo.com>
2022-01-31 16:54:30 -0600

    Completed Question 1. Exceeded Maximum Recursion Depth.

--- Part2.py
+++ Part2.py
@@ -2,7 +2,6 @@
 
 # Problem #1 - Happy Numbers
 
-from itertools import count
 
 
 happy_number = input("Enter a number to see if it's happy! ")
@@ -16,32 +15,33 @@ def second_mode(broken_integer):
         next_step.append(int(number * number))
 
 def numbers(next_step):
-    count == 0
+    last_count = 0
     for number in next_step:
-        count == count + number
-        return count
+        last_count += number
+    return last_count
 
 
-def final_check(number_sum):
-    if number_sum != 1:
-        broken_integer = [number_sum]
+def final_check(final_sum):
+    if final_sum != 1:
+        broken_integer = []
         next_step.clear()
-        first_mode(number_sum)
+        first_mode(final_sum)
         second_mode(broken_integer)
-        numbers(next_step)
+        final_sum = numbers(next_step)
         final_check(final_sum)
-    elif number_sum == happy_number:
-        print("Your number is not a happy number.")
-    elif number_sum == 1:
+    elif final_sum == 1:
         print("Your number is a happy number!")
+    elif final_sum == happy_number:
+        print("Your number is not a happy number.")
+    elif final_sum == final_sum:
+        print('Your number is not a happy number.')
 
 broken_integer = []
 next_step = []
-final_sum = numbers(next_step)
 
 first_mode(happy_number)
 second_mode(broken_integer)
-numbers(next_step)
+final_sum = numbers(next_step)
 final_check(final_sum)
 
 
