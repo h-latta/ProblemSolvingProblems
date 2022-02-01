4cf048ca5384cb3b58aa5917a9767d4b19be2251
h-latta <h.latta@yahoo.com>
2022-02-01 15:44:58 -0600

    Started Part 3

---
+++ Part3.py
@@ -0,0 +1,41 @@
+# Problem Solving Problems part 2!
+# WHITEBOARD CHALLENGES!
+
+
+#Problem #1 - Target Number in an Array
+
+
+
+#Problem #2 - Palindromes
+
+
+
+#Problem #3 - Incrementing Integers
+
+
+
+#Problem #4 - Positive Sum, Negative Sum
+
+
+
+#Problem #5 - High and Low Numbers
+
+
+
+#Problem #6 - Email Validation
+
+
+
+#Problem #7 - Letters to Numbers
+
+
+
+#Problem #8 - Rolling Lock
+
+
+
+#Problem #9 - Happy Numbers Part 2
+
+
+
+#Problem #10 - Reciprocal of the Reverse
